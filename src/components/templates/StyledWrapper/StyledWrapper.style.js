ed020ba76064e68b5df718166c76e6d7cd687ea8
trak2026z <trak2026z@gmail.com>
2025-08-09 12:14:52 +0200

    Add StyledWrapper styled-component

---
+++ src/components/templates/StyledWrapper/StyledWrapper.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 1200px;
+`;
