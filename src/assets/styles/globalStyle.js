fcbafdcb17556f8573ea2d91574095ccfc0ba8e3
trak2026z <trak2026z@gmail.com>
2025-08-09 12:13:24 +0200

    Add GlobalStyle for styled-components

---
+++ src/assets/styles/globalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+  }
+`;
