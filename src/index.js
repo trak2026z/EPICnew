bbbf99843f345f213c414e240ac84f466b28f476
trak2026z <trak2026z@gmail.com>
2025-08-09 13:56:49 +0200

    Update index.js to render App instead of Root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Root from './Root';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>
-);
+I); 
